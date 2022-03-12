c43f836d3ba9ff266b0cce564f10c7aa0c39a4fe
Raghu <raghu.k@techouts.com>
2022-03-12 15:28:09 +0530

    HashMap task

---
+++ OopsTasks/src/org/hashmap/java/HashMapSort.java
@@ -0,0 +1,18 @@
+package org.hashmap.java;
+
+import java.util.*;
+
+public class HashMapSort {
+
+	public static void main(String[] args) {
+		HashMap <Integer,String> map=new HashMap<>();
+		map.put(1,"virat");
+		map.put(2,"pant");
+		map.put(3,"rohit");
+		map.put(4,"jaddu");
+		map.put(5,"rahul");
+		map.put(6,"boom");
+		map.put(7,"siraj");
+	}
+
+}
