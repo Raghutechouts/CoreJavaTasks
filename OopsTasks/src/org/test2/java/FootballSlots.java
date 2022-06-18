70e6654e1d1b98a0b7f23541984af0fc42a799cb
Raghutechouts <99718587+Raghutechouts@users.noreply.github.com>
2022-06-18 17:50:11 +0530

    Update FootballSlots.java

--- OopsTasks/src/org/test2/java/FootballSlots.java
+++ OopsTasks/src/org/test2/java/FootballSlots.java
@@ -8,27 +8,34 @@ public class FootballSlots {
 
 
 	static HashMap <Integer,String> footballFilledSlots=new HashMap<>();
-	static HashMap <Integer,String> footballTiming=new HashMap<>();{
-		footballTiming.put(1,"08:00-08:30");
-		footballTiming.put(2,"08:30-09:00");
-		footballTiming.put(3,"09:00-09:30");
-		footballTiming.put(4,"09:30-10:00");
-		footballTiming.put(5,"10:00-10:30");
-		footballTiming.put(6,"10:30-11:00");
-		footballTiming.put(8,"11:00-11:30");
-		footballTiming.put(9,"11:30-12:00");
-		footballTiming.put(10,"12:00-12:30");
-		footballTiming.put(11,"15:00-15:30");
-		footballTiming.put(12,"15:30-16:00");
-		footballTiming.put(13,"16:00-16:30");
-		footballTiming.put(14,"16:30-17:00");
-		footballTiming.put(15,"17:00-17:30");
-		footballTiming.put(16,"17:30-18:00");
-		footballTiming.put(18,"18:00-18:30");
-		footballTiming.put(19,"18:30-20:00");
-		footballTiming.put(20,"20:00-20:30");
+	static HashMap <Integer,String> footballTiming=new HashMap<>();
+// 	{
+// 		footballTiming.put(1,"08:00-08:30");
+// 		footballTiming.put(2,"08:30-09:00");
+// 		footballTiming.put(3,"09:00-09:30");
+// 		footballTiming.put(4,"09:30-10:00");
+// 		footballTiming.put(5,"10:00-10:30");
+// 		footballTiming.put(6,"10:30-11:00");
+// 		footballTiming.put(8,"11:00-11:30");
+// 		footballTiming.put(9,"11:30-12:00");
+// 		footballTiming.put(10,"12:00-12:30");
+// 		footballTiming.put(11,"15:00-15:30");
+// 		footballTiming.put(12,"15:30-16:00");
+// 		footballTiming.put(13,"16:00-16:30");
+// 		footballTiming.put(14,"16:30-17:00");
+// 		footballTiming.put(15,"17:00-17:30");
+// 		footballTiming.put(16,"17:30-18:00");
+// 		footballTiming.put(18,"18:00-18:30");
+// 		footballTiming.put(19,"18:30-20:00");
+// 		footballTiming.put(20,"20:00-20:30");
+// 	}
+	int count1=8;
+	public void fillTimimgsForFootball(){
+		 for (int i=1;i<=20;i++) {
+			 footballTiming.put(i, toString(count1)+":00"+":"+toString(count1)+":30");
+			 footballTiming.put(++i, toString(count1)+":30"+":"+toString(++count1)+":00");
+		 }
 	}
-
 	public void availableSlotsFootball() {
 		System.out.println("For Football available slots are: ");
 		for(Map.Entry m : footballTiming.entrySet()){    
