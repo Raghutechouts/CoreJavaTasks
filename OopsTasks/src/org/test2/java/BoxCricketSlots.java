3503768e505a974522369cf385859d8359893b5b
Raghutechouts <99718587+Raghutechouts@users.noreply.github.com>
2022-06-18 17:53:31 +0530

    Update BoxCricketSlots.java

--- OopsTasks/src/org/test2/java/BoxCricketSlots.java
+++ OopsTasks/src/org/test2/java/BoxCricketSlots.java
@@ -9,14 +9,21 @@ public class BoxCricketSlots {
 
 	
 	static HashMap <Integer,String> cricketFilledSlots=new HashMap<>();
-	static HashMap <Integer,String> cricketTiming=new HashMap<>();{
-		cricketTiming.put(1,"08:00-10:00");
-		cricketTiming.put(2,"10:00-12:00");
-		cricketTiming.put(3,"14:00-16:00");
-		cricketTiming.put(4,"16:00-18:00");
-		cricketTiming.put(5,"18:00-20:00");
+	static HashMap <Integer,String> cricketTiming=new HashMap<>();
+// 	{
+// 		cricketTiming.put(1,"08:00-10:00");
+// 		cricketTiming.put(2,"10:00-12:00");
+// 		cricketTiming.put(3,"14:00-16:00");
+// 		cricketTiming.put(4,"16:00-18:00");
+// 		cricketTiming.put(5,"18:00-20:00");
+// 	}
+	int count=9;
+	public void fillTimimgsForCricket(){
+		 for (int i=1;i<=5;i++) {
+			 cricketTiming.put(i, toString(++count2)+":00"+":"+toString(++(count2)+1)+":00");
+			 cricketTiming.put(++i, toString(++count2)+":00"+":"+toString(++(count2)+1)+":00");
+		 }
 	}
-	
 	public void availableSlotsCricket() {
 		System.out.println("For Cricket available slots are: ");
 		for(Map.Entry m : cricketTiming.entrySet()){    
